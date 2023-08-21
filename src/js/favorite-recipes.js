4e519a205274a3918a94ae9a88e4951d939fd176
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-08-21 21:46:39 +0300

    fix ... in popular recipes

--- src/js/favorite-recipes.js
+++ src/js/favorite-recipes.js
@@ -12,13 +12,14 @@ fetch('https://tasty-treats-backend.p.goit.global/api/recipes/popular')
   .then(data => {
     const recipe = data
       .map(data => {
-        return `<li class="popular-recipe-list-li" data-recipe-id="${data._id}">
+        const dataDesctiptionSlice = data.description.slice(0, 75);
+        return `<li class="popular-recipe-list-li" data-recipe-id="${data._id}>
         <div class="favorite-list-img">
         <img class="round-img-1" src="${data.preview}" alt="${data.title}">
       </div>
         <div class="popular-recipe-div-list">
           <h2 class="favorite-list-h2">${data.title}</h2>
-          <h3 class="favorite-list-h3" >${data.description}</h3>
+          <h3 class="favorite-list-h3" >${dataDesctiptionSlice}...</h3>
         </div>
         </li>`;
       })
