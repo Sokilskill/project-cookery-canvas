33ae99cf99e9fae2d05ba9cfa42c25be01f70b18
Sokilskill <126295444+Sokilskill@users.noreply.github.com>
2023-08-21 10:33:07 +0000

    Deploying to gh-pages from @ Sokilskill/project-cookery-canvas@1b38f017162caa4d0dea57393a1a25af18d4f9b8 🚀

---
+++ favorites.e2d04fcb.js
@@ -0,0 +1,2 @@
+!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},r={},o=e.parcelRequiref774;null==o&&((o=function(e){if(e in n)return n[e].exports;if(e in r){var o=r[e];delete r[e];var i={id:e,exports:{}};return n[e]=i,o.call(i.exports,i,i.exports),i.exports}var f=new Error("Cannot find module '"+e+"'");throw f.code="MODULE_NOT_FOUND",f}).register=function(e,n){r[e]=n},e.parcelRequiref774=o),o("cs7FV"),o("32ZrB"),o("9Di7p")}();
+//# sourceMappingURL=favorites.e2d04fcb.js.map
